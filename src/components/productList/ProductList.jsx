0f32bb51669f17ca6a0cc1f7ad3d04c5c41f2875
Vanessa Moreno Torres <87245201+VanessaMorenoTorres@users.noreply.github.com>
2022-01-05 13:27:16 -0500

    intro and about
    intro and about components

---
+++ src/components/productList/ProductList.jsx
@@ -0,0 +1,23 @@
+import Product from "../product/Product"
+import "./productList.css"
+import { products} from "../../data"
+
+const ProductList = () => {
+    return (
+        <div className="pl">
+            <div className="pl-text">
+                <h1 className="pl-title">Placeholder Title (Create & Inspire?)</h1>
+                <p className="pldesc">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid voluptatibus corrupti omnis neque minima laboriosam corporis voluptates exercitationem, expedita maiores consequatur deserunt iure nesciunt nam, iusto vero eveniet, reprehenderit temporibus.
+                </p>
+            </div>
+            <div className="pl-list">
+                {products.map((item) => (
+                    <Product key={item.id} img={item.img}/>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ProductList
