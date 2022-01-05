106f487116615e652f075fb6adb8529e61df20b8
Vanessa Moreno Torres <87245201+VanessaMorenoTorres@users.noreply.github.com>
2022-01-05 13:32:25 -0500

    Revert "git ignore"
    This reverts commit cd2ea68c001cd733c0f1a56d3abbcf1d2f0c7bdb.

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/9887155/pexels-photo-9887155.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        link: "https://off-the-shelf.vercel.app/",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/9887155/pexels-photo-9887155.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        link: "https://github.com/VanessaMorenoTorres",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/9887155/pexels-photo-9887155.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        link: "https://github.com/VanessaMorenoTorres",
+    },
+    {
+        id: 4,
+        img: "https://images.pexels.com/photos/9887155/pexels-photo-9887155.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        link: "https://github.com/VanessaMorenoTorres",
+    },
+    {
+        id: 5,
+        img: "https://images.pexels.com/photos/9887155/pexels-photo-9887155.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        link: "https://github.com/VanessaMorenoTorres",
+    },
+    {
+        id: 6,
+        img: "https://images.pexels.com/photos/9887155/pexels-photo-9887155.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+        link: "https://github.com/VanessaMorenoTorres",
+    }
+]
