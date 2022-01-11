d8a534c421f33aadfd298699fe7270e1744eacc9
Vanessa Moreno Torres <87245201+VanessaMorenoTorres@users.noreply.github.com>
2022-01-11 12:43:10 -0500

    create contact form
    only format, not yet functional

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -25,7 +25,16 @@ const Contact = () => {
                         </div>
                 </div>
                 <div className="c-right">
-
+                    <p className='c-desc'>
+                        <b>What's your story?</b> Get in touch, how can I help you? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi in odit repellendus ex consequatur tempora. Laudantium, iusto rem nesciunt, velit ipsam impedit dignissimos dolor mollitia recusandae officiis illum, obcaecati eum!
+                    </p>
+                    <form>
+                        <input type="text" placeholder='Name' name='user_name'/>
+                        <input type="text" placeholder='Subject' name='user_subject'/>
+                        <input type="text" placeholder='Email' name='user_email'/>
+                        <textarea name="message" placeholder='Message' id="" cols="30" rows="5"></textarea>
+                        <button>Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
