57a9ec80a5ff99f31e3a42b98a56034049c83055
Vanessa Moreno Torres <87245201+VanessaMorenoTorres@users.noreply.github.com>
2022-01-14 11:37:54 -0500

    Begin Router Set Up
    Not complete

---
+++ src/components/navigation/Navigation.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom"
+import Contact from "../contact/Contact";
+import Intro from "../intro/Intro";
+import About from "../about/About";
+import ProductList from "../productList/ProductList";
+
+
+function Navigation(){
+    return(
+        <Router>
+                  <div className="navBar">
+            <nav>
+          <ul className="pageLinks">
+            <li>
+              <Link to="/">Intro</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link></li>
+          </ul>
+          </nav>
+          <Switch>
+            <Route path="/projects" component={ProductList} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/" component={Intro} />
+          </Switch>
+          </div> 
+        </Router>
+    )
+}
+
+export default Navigation
