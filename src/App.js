3e6f3c1ddb8a5403064dcd5b63a15c31c75c4339
Vanessa Moreno Torres <87245201+VanessaMorenoTorres@users.noreply.github.com>
2022-01-18 13:41:40 -0500

    added bootstrap
    complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
 import ProductList from "./components/productList/ProductList";
